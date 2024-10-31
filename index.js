c292487d76c85a6fde95e966944a0189be0eac41
Nova <37310850+NovaProton@users.noreply.github.com>
2024-10-31 17:11:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3000;
 // URLs for the two images
 const LOCAL_EUMETSAT_URL = 'https://view.eumetsat.int/geoserver/wms?service=WMS&version=1.3.0&request=GetMap&TRANSPARENT=True&WIDTH=488&HEIGHT=487&BBOX=-62.8,-15.9,51.9,71.8&FORMAT=image/jpeg&LAYERS=mtg_fd:rgb_geocolour';
 const WORLD_EUMETSAT_URL = 'https://view.eumetsat.int/geoserver/ows?access_token=311845d4-6c48-3832-8bed-546ef3284087&service=WMS&request=GetMap&version=1.3.0&layers=mtg_fd:rgb_geocolour&styles=&format=image/jpeg&crs=EPSG:4326&bbox=-77.3506393432617,-81.2777938842773,77.3563919067383,81.2807235717773&width=800&height=761';
+const HIMAWARI_URL = 'https://www.himawari8.nict.go.jp/img/D531106/latest.jpg'; // Replace with the latest available Himawari URL if needed
+
 
 // Route to serve the local image
 app.get('/local', async (req, res) => {
@@ -39,6 +41,22 @@ app.get('/world', async (req, res) => {
     } catch (error) {
         console.error('Error fetching the world image:', error);
         res.status(500).send('Error fetching the world image');
+    }
+});
+
+app.get('/himawari', async (req, res) => {
+    try {
+        const response = await axios({
+            url: HIMAWARI_URL,
+            method: 'GET',
+            responseType: 'arraybuffer'
+        });
+
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.send(response.data);
+    } catch (error) {
+        console.error("Error fetching Himawari-8 image:", error);
+        res.status(500).send("Error fetching Himawari-8 image");
     }
 });
 
